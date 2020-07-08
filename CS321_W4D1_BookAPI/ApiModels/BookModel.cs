8eb432a91bcc4ad799ae872b5746760c927bde2b
mercesolis <mersolis@live.com>
2020-07-07 23:20:48 -0500

    BookAPI APIModel

--- CS321_W4D1_BookAPI/ApiModels/BookModel.cs
+++ CS321_W4D1_BookAPI/ApiModels/BookModel.cs
@@ -7,5 +7,15 @@ namespace CS321_W4D1_BookAPI.ApiModels
     {
         public int Id { get; set; }
         // TODO: fill in BookModel properties
+        public string Title { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string Genre { get; set; }
+        public int PublicationYear { get; set; }
+
+        public int AuthorId { get; set; }
+        public string Author { get; set; }
+
+        public int PublisherId { get; set; }
+
     }
 }
